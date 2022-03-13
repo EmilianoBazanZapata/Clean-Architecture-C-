bdf823e8a4578443ce0f8b5eb08db202f6626b22
Emiliano Bazan-Zapata <emilianobz546@gmail.com>
2022-03-13 17:37:09 -0300

    Modificando la Estructura de la Aplicacion

--- .NET CAPACITACION/CleanAcrchitecture.Domain/Entities/VideoActor.cs
+++ .NET CAPACITACION/CleanAcrchitecture.Domain/Entities/VideoActor.cs
@@ -1,6 +1,8 @@
+using CleanAcrchitecture.Domain.Common;
+
 namespace CleanAcrchitecture.Domain.Entities
 {
-    public class VideoActor
+    public class VideoActor : BaseDomainModel
     {
         public int VideoId { get; set; }
         public int ActorId { get; set; }
